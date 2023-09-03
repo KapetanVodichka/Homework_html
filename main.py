e0e2ae4320610285a3774fa81059a0fb7dce26b0
Sergey <kapetanvodichka@gmail.com>
2023-09-03 10:39:36 +0500

    Homework | html-file reworked №2

--- main.py
+++ main.py
@@ -296,55 +296,61 @@ class MyServer(BaseHTTPRequestHandler):
                                         </p>
                                         <button type="button" class="btn btn-lg btn-success">Купить</button>
                                     </div>
-                                    <form class="ms-4">
-                                        <div class="mb-3">
-                                            <label for="name" class="form-label">Имя</label>
-                                            <input type="text" class="form-control" id="name" required>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email</label>
-                                            <input type="email" class="form-control" id="email" required>
-                                        </div>
-                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                            <div class="col">
-                                <h2 class="mb-4">FAQ</h2>
-                                <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-                                    Как купить?
-                                </button>
-                                <div class="collapse" id="collapse1">
-                                    <div class="card card-body">
-                                        Легко!
+                    <div class="col-4">
+                        <div class="card mb-4 rounded-3 shadow-sm">                        
+                            <div class="card-body d-flex flex-column">
+                                <form class="ms-4">
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Name</label>
+                                        <input type="text" class="form-control" id="name" required>
                                     </div>
-                                </div>
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="email" required>
+                                    </div>
+                                    <button type="button" class="btn btn-lg btn-success">Отправить</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2 class="mb-4">FAQ</h2>
+                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                            Как купить?
+                        </button>
+                        <div class="collapse" id="collapse1">
+                            <div class="card card-body">
+                                Легко!
+                            </div>
+                        </div>
                                 
-                                <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                                    Как доставить?
-                                </button>
-                                <div class="collapse" id="collapse2">
-                                    <div class="card card-body">
-                                        Быстро!
-                                    </div>
-                                </div>
-                                
-                                <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                                    Какая гарантия?
-                                </button>
-                                <div class="collapse" id="collapse3">
-                                    <div class="card card-body">
-                                        На всё воля божья :3
-                                    </div>
-                                </div>
+                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                            Как доставить?
+                        </button>
+                        <div class="collapse" id="collapse2">
+                            <div class="card card-body">
+                                Быстро!
+                            </div>
+                        </div>
+                            
+                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                            Какая гарантия?
+                        </button>
+                        <div class="collapse" id="collapse3">
+                            <div class="card card-body">
+                                На всё воля божья :3
                             </div>
                         </div>
                     </div>
                 </div>
-            </main>
-        </div>
+            </div>
+        </main>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
